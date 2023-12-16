b80a358fb9ce3594a39b6017aacc437abc5d1c0d
Alessandro <alessandronobre.ti@gmail.com>
2023-12-15 22:00:22 -0300

    Primeiro commit ms cadastro

---
+++ mscadastro/src/main/java/com/devpro/mscadastro/MscadastroApplication.java
@@ -0,0 +1,13 @@
+package com.devpro.mscadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscadastroApplication.class, args);
+	}
+
+}
---
+++ mscadastro/src/test/java/com/devpro/mscadastro/MscadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpro.mscadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
